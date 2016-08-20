ddd84b55045ae4062d61eca2dcbb98233384c4f6
rexbut <butbutdu45@gmail.com>
2016-08-20 18:24:32 +0200

    Update
    Signed-off-by: rexbut <butbutdu45@gmail.com>

--- src/main/java/fr/evercraft/everworlds/EWPermissions.java
+++ src/main/java/fr/evercraft/everworlds/EWPermissions.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of EverSigns.
+ * This file is part of EverWorlds.
  *
- * EverSigns is free software: you can redistribute it and/or modify
+ * EverWorlds is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * EverSigns is distributed in the hope that it will be useful,
+ * EverWorlds is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with EverSigns.  If not, see <http://www.gnu.org/licenses/>.
+ * along with EverWorlds.  If not, see <http://www.gnu.org/licenses/>.
  */
 package fr.evercraft.everworlds;
 
